7598a7a95e6173b13e9689d9bb948156e1f0f600
hannesblaman <hannes@blaman.com>
2024-08-27 12:19:51 +0200

    While loop

--- SY22_PRRPRR02_02_Repetition_2/Program.cs
+++ SY22_PRRPRR02_02_Repetition_2/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace SY22_PRRPRR02_02_Repetition_2 {
 	internal class Program {
 		static void Main(string[] args) {
+
+			int i = 0;
+
+			while (i < 10) {
+				Console.WriteLine("Tjena #" + i);
+				//i++;
+			}
 		}
 	}
 }
