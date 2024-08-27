7b53c6ecbba8dd57b3cbf0ee457717700b1c6ef3
hannesblaman <hannes@blaman.com>
2024-08-27 13:21:15 +0200

    Övningar 3 - Checkout

--- SY22_PRRPRR02_02_Repetition_2/Program.cs
+++ SY22_PRRPRR02_02_Repetition_2/Program.cs
@@ -8,13 +8,22 @@ namespace SY22_PRRPRR02_02_Repetition_2 {
 	internal class Program {
 		static void Main(string[] args) {
 
-			
-			for (int i = 10; i > 0; i--) {
+			Console.WriteLine("Mata in ditt första tal!");
+			string input1 = Console.ReadLine();
+			int num1 = int.Parse(input1);
+
+			Console.WriteLine("Mata in ditt andra tal!");
+			string input2 = Console.ReadLine();
+			int num2 = int.Parse(input2);
+
+			for (int i = num1; i < num2; i++) {
+				Console.Write(i);
 				for (int j = 0; j < i; j++) {
 					Console.Write('*');
 				}
 				Console.WriteLine();
 			}
+			
 		}
 	}
 }
